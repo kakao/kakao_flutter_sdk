1da8aaa20af6b84ce7fc64284373a236c92a8adb
Hara Kang <hara0115@gmail.com>
2019-08-14 18:37:07 +0900

    add dartdoc script for applying several options

--- doc.sh
+++ doc.sh
@@ -1,3 +1,1 @@
-dartdoc --exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:io,dart:isolate,dart:math,dart:typed_data,dart:ui'
-mv doc/api docs/
-rmdir doc/
+dartdoc --exclude 'dart:async,dart:collection,dart:convert,dart:core,dart:developer,dart:io,dart:isolate,dart:math,dart:typed_data,dart:ui'  --link-to-remote --output docs
