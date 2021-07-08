04972ab2e8006f857969d1de1f44537ced36a79e
mbkim95 <mbkim95@naver.com>
2021-07-08 15:38:53 +0900

    fix navigateWebUrl()
    - fix to work on iOS

--- lib/src/navi/navi_api.dart
+++ lib/src/navi/navi_api.dart
@@ -7,12 +7,15 @@ import 'package:kakao_flutter_sdk/src/common/api_factory.dart';
 import 'package:kakao_flutter_sdk/src/navi/model/kakao_navi_params.dart';
 import 'package:kakao_flutter_sdk/src/navi/model/location.dart';
 import 'package:kakao_flutter_sdk/src/navi/model/navi_option.dart';
+import 'package:platform/platform.dart';
 
 /// Provides KakaoTalk API.
 class NaviApi {
-  NaviApi(this._dio);
+  NaviApi(this._dio, {Platform? platform})
+      : _platform = platform ?? LocalPlatform();
 
   final Dio _dio;
+  final Platform _platform;
   final MethodChannel _channel = MethodChannel("kakao_flutter_sdk");
   static const String NAVI_HOSTS = "kakaonavi-wguide.kakao.com";
 
@@ -29,10 +32,17 @@ class NaviApi {
       {NaviOption? option, List<Location>? viaList}) async {
     final naviParams =
         KakaoNaviParams(location, option: option, viaList: viaList);
+    print('ios: ${_platform.isIOS}');
     final extras = {
-      'appPkg': await KakaoContext.packageName,
-      'keyHash': await KakaoContext.origin,
-      'KA': await KakaoContext.kaHeader
+      'KA': await KakaoContext.kaHeader,
+      ...(_platform.isAndroid
+          ? {
+              "appPkg": await KakaoContext.packageName,
+              "keyHash": await KakaoContext.origin
+            }
+          : _platform.isIOS
+              ? {"appPkg": await KakaoContext.origin}
+              : {})
     };
     final params = {
       'param': jsonEncode(naviParams),
