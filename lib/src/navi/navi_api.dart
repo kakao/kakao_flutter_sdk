1aaa7e1d8f0baf48b0a3a80a5dd979f6c390e4d1
mbkim95 <mbkim95@naver.com>
2021-07-08 12:18:57 +0900

    remove uri in pubspec.yaml

--- lib/src/navi/navi_api.dart
+++ lib/src/navi/navi_api.dart
@@ -1,13 +1,10 @@
 import 'package:dio/dio.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:uri/uri.dart';
-import 'package:json_annotation/json_annotation.dart';
 import 'package:kakao_flutter_sdk/common.dart';
 import 'package:kakao_flutter_sdk/src/common/api_factory.dart';
+import 'package:kakao_flutter_sdk/src/navi/model/kakao_navi_params.dart';
 import 'package:kakao_flutter_sdk/src/navi/model/location.dart';
 import 'package:kakao_flutter_sdk/src/navi/model/navi_option.dart';
-import 'package:kakao_flutter_sdk/src/navi/model/kakao_navi_params.dart';
 
 /// Provides KakaoTalk API.
 class NaviApi {
@@ -15,7 +12,7 @@ class NaviApi {
 
   final Dio _dio;
   final MethodChannel _channel = MethodChannel("kakao_flutter_sdk");
-  final String NAVI_HOSTS = "kakaonavi-wguide.kakao.com";
+  static const String NAVI_HOSTS = "kakaonavi-wguide.kakao.com";
 
   /// Default instance SDK provides.
   static final NaviApi instance = NaviApi(ApiFactory.dapi);
@@ -26,21 +23,21 @@ class NaviApi {
     return isInstalled;
   }
 
-  Future<Uri?> navigateWebUrl(Location location, NaviOption? option, List<Location>? viaList) async {
-
-    final param = KakaoNaviParams(location, option, viaList);
-    final extras = await _channel.invokeMethod<String>("getKaHeader");
-    final builder = UriBuilder()
-      ..scheme = 'https'
-      ..host = NAVI_HOSTS
-      ..port = 443
-      ..path = 'navigate.html'
-      ..queryParameters['param'] = param.toJson().toString()
-      ..queryParameters['apiver'] = '1.0'
-      ..queryParameters['appkey'] = KakaoContext.clientId
-      ..queryParameters['extras'] = extras.toString();
-
-    final naviUri = builder.build();
-    return naviUri;
+  Future<Uri?> navigateWebUrl(Location location,
+      {NaviOption? option, List<Location>? viaList}) async {
+    final naviParams =
+        KakaoNaviParams(location, option: option, viaList: viaList);
+    final extras = {
+      'appPkg': KakaoContext.packageName,
+      'keyHash': KakaoContext.origin,
+      'KA': KakaoContext.kaHeader
+    };
+    final params = {
+      'param': naviParams.toString(),
+      'apiver': '1.0',
+      'appkey': KakaoContext.clientId,
+      'extras': extras.toString()
+    };
+    return Uri.https(NAVI_HOSTS, 'navigate.html', params);
   }
 }
