d3b87c1fdaa4d26443d68d86f34e5469a862f417
mbkim95 <mbkim95@naver.com>
2021-08-06 09:47:59 +0900

    migrate from package_info to package_info_plus

--- lib/src/common/kakao_context.dart
+++ lib/src/common/kakao_context.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
-import 'package:package_info/package_info.dart';
+import 'package:package_info_plus/package_info_plus.dart';
 
 /// Singleton context for Kakao Flutter SDK.
 class KakaoContext {
