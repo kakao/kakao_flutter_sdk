8d61a0fa7f94baff5c4dec9eb853ab813aff9b71
mbkim95 <mbkim95@naver.com>
2021-10-01 20:51:37 +0900

    Add token migration test code

--- test/auth/token_manager_test.dart
+++ test/auth/token_manager_test.dart
@@ -3,6 +3,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:kakao_flutter_sdk/src/auth/model/access_token_response.dart';
 import 'package:kakao_flutter_sdk/src/auth/model/oauth_token.dart';
 import 'package:kakao_flutter_sdk/src/auth/token_manager.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../helper.dart';
 
@@ -48,4 +49,72 @@ void main() {
     var newToken = await tokenManager.getToken();
     expect(null, newToken);
   });
+
+  test("token migration test", () async {
+    var oldTokenManager = OldTokenManager();
+    await oldTokenManager.setToken(OAuthToken.fromResponse(response));
+    final oldToken = await oldTokenManager.getToken();
+    expect(true, oldToken != null);
+    final newToken = await tokenManager.getToken();
+    expect(true, newToken != null);
+
+    try {
+      final prevToken = await oldTokenManager.getToken();
+      fail("should not reach here");
+    } catch (e) {}
+  });
+}
+
+const tokenKey = "com.kakao.token.OAuthToken";
+const atKey = "com.kakao.token.AccessToken";
+const atExpiresAtKey = "com.kakao.token.AccessToken.ExpiresAt";
+const rtKey = "com.kakao.token.RefreshToken";
+const rtExpiresAtKey = "com.kakao.token.RefreshToken.ExpiresAt";
+const secureModeKey = "com.kakao.token.KakaoSecureMode";
+const scopesKey = "com.kakao.token.Scopes";
+
+// old version token manager ( ~ 0.8.2)
+class OldTokenManager implements TokenManager {
+  @override
+  Future<void> clear() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.remove(atKey);
+    await preferences.remove(atExpiresAtKey);
+    await preferences.remove(rtKey);
+    await preferences.remove(rtExpiresAtKey);
+    await preferences.remove(secureModeKey);
+    await preferences.remove(scopesKey);
+  }
+
+  @override
+  Future<OAuthToken?> getToken() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    var accessToken = preferences.getString(atKey);
+    var atExpiresAtMillis = preferences.getInt(atExpiresAtKey);
+
+    var accessTokenExpiresAt = atExpiresAtMillis != null
+        ? DateTime.fromMillisecondsSinceEpoch(atExpiresAtMillis)
+        : null;
+    var refreshToken = preferences.getString(rtKey);
+    var rtExpiresAtMillis = preferences.getInt(rtExpiresAtKey);
+    var refreshTokenExpiresAt = rtExpiresAtMillis != null
+        ? DateTime.fromMillisecondsSinceEpoch(rtExpiresAtMillis)
+        : null;
+    List<String>? scopes = preferences.getStringList(scopesKey);
+
+    return OAuthToken(accessToken!, accessTokenExpiresAt!, refreshToken!,
+        refreshTokenExpiresAt!, scopes);
+  }
+
+  @override
+  Future<void> setToken(OAuthToken token) async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    await preferences.setString(atKey, token.accessToken);
+    await preferences.setInt(
+        atExpiresAtKey, token.accessTokenExpiresAt.millisecondsSinceEpoch);
+    await preferences.setString(rtKey, token.refreshToken);
+    await preferences.setInt(
+        rtExpiresAtKey, token.refreshTokenExpiresAt.millisecondsSinceEpoch);
+    await preferences.setStringList(scopesKey, token.scopes!);
+  }
 }
