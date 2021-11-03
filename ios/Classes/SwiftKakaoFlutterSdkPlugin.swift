37aa06c93ea6ee2d9c464089e650f7297a408e73
mbkim95 <mbkim95@naver.com>
2021-11-03 11:13:52 +0900

    Add shareDestination() api for iOS

--- ios/Classes/SwiftKakaoFlutterSdkPlugin.swift
+++ ios/Classes/SwiftKakaoFlutterSdkPlugin.swift
@@ -61,7 +61,14 @@ public class SwiftKakaoFlutterSdkPlugin: NSObject, FlutterPlugin, ASWebAuthentic
             let appKey = args["app_key"]
             let extras = args["extras"]
             let params = args["navi_params"]
-            let url = Utility.makeUrlWithParameters("kakaonavi-sdk://navigate", parameters: ["extras": extras!, "param": params!, "appkey": appKey!, "apiver": "1.0"])            
+            let url = Utility.makeUrlWithParameters("kakaonavi-sdk://navigate", parameters: ["extras": extras!, "param": params!, "appkey": appKey!, "apiver": "1.0"])
+            UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+        case "shareDestination":
+            let args = call.arguments as! Dictionary<String, String>
+            let appKey = args["app_key"]
+            let extras = args["extras"]
+            let params = args["navi_params"]
+            let url = Utility.makeUrlWithParameters("kakaonavi-sdk://sharePoi", parameters: ["extras": extras!, "param": params!, "appkey": appKey!, "apiver": "1.0"])
             UIApplication.shared.open(url!, options: [:], completionHandler: nil)
         default:
             result(FlutterMethodNotImplemented)
