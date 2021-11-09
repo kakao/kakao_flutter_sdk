b9f83ffc6ebb87ca921e74367738a0e04e6ed006
mbkim95 <mbkim95@naver.com>
2021-11-09 16:16:32 +0900

    Translate the reference of the talk package into Korean

--- lib/src/talk/model/channel_relation.dart
+++ lib/src/talk/model/channel_relation.dart
@@ -2,22 +2,30 @@ import 'package:json_annotation/json_annotation.dart';
 
 part 'channel_relation.g.dart';
 
+/// 카카오톡 채널 추가상태 정보.
 @JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
 class ChannelRelation {
+  /// 채널의 uuid
   @JsonKey(name: "channel_uuid")
   String uuid;
+
+  /// encoded channel public id (ex. https://pf.kakao.com/${channelId})
   @JsonKey(name: "channel_public_id")
   String publicId;
+
+  /// 사용자의 채널 추가 상태
   String relation;
+
+  /// 마지막 상태 변경 일시 (현재는 ADDED 상태의 친구 추가시각만 의미)
   DateTime? updatedAt;
 
-  /// <nodoc>
+  /// @nodoc
   ChannelRelation(this.uuid, this.publicId, this.relation, this.updatedAt);
 
-  /// <nodoc>
+  /// @nodoc
   factory ChannelRelation.fromJson(Map<String, dynamic> json) =>
       _$ChannelRelationFromJson(json);
 
-  /// <nodoc>
+  /// @nodoc
   Map<String, dynamic> toJson() => _$ChannelRelationToJson(this);
 }
--- lib/src/talk/model/channel_relations.dart
+++ lib/src/talk/model/channel_relations.dart
@@ -3,19 +3,23 @@ import 'package:kakao_flutter_sdk/src/talk/model/channel_relation.dart';
 
 part 'channel_relations.g.dart';
 
+/// 카카오톡 채널 추가상태 조회 API 응답 클래스
 @JsonSerializable(
     fieldRename: FieldRename.snake, explicitToJson: true, includeIfNull: false)
 class ChannelRelations {
+  /// 사용자 아이디
   int? userId;
+
+  /// 사용자의 채널 추가상태 목록
   List<ChannelRelation> channels;
 
-  /// <nodoc>
+  /// @nodoc
   ChannelRelations(this.userId, this.channels);
 
-  /// <nodoc>
+  /// @nodoc
   factory ChannelRelations.fromJson(Map<String, dynamic> json) =>
       _$ChannelRelationsFromJson(json);
 
-  /// <nodoc>
+  /// @nodoc
   Map<String, dynamic> toJson() => _$ChannelRelationsToJson(this);
 }
