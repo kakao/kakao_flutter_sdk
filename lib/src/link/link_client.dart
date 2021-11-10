cd92a08f62f3a8613395b071aaf387b338cebb55
mbkim95 <mbkim95@naver.com>
2021-11-10 17:57:56 +0900

    Revert "Modify method access modifier in LinkClient"
    This reverts commit 163e70f40e9738266c0fe2999fd97a1b50d97b42.

--- lib/src/link/link_client.dart
+++ lib/src/link/link_client.dart
@@ -11,7 +11,6 @@ import 'package:kakao_flutter_sdk/src/link/model/link_result.dart';
 import 'package:kakao_flutter_sdk/src/template/default_template.dart';
 import 'package:platform/platform.dart';
 
-/// 카카오링크 호출을 담당하는 클라이언트.
 class LinkClient {
   LinkClient(this.api, {Platform? platform})
       : _platform = platform ?? LocalPlatform();
@@ -20,7 +19,7 @@ class LinkClient {
 
   static final MethodChannel _channel = MethodChannel("kakao_flutter_sdk");
 
-  /// 간편한 API 호출을 위해 기본 제공되는 singleton 객체
+  /// singleton instance of this class.
   static final LinkClient instance = LinkClient(LinkApi.instance);
 
   Future<bool> isKakaoLinkAvailable() async {
@@ -32,15 +31,13 @@ class LinkClient {
       {Map<String, String>? templateArgs,
       Map<String, String>? serverCallbackArgs}) async {
     final response = await api.custom(templateId, templateArgs: templateArgs);
-    return _sharerWithResponse(response,
-        serverCallbackArgs: serverCallbackArgs);
+    return sharerWithResponse(response, serverCallbackArgs: serverCallbackArgs);
   }
 
   Future<Uri> defaultWithWeb(DefaultTemplate template,
       {Map<String, String>? serverCallbackArgs}) async {
     final response = await api.defaultTemplate(template);
-    return _sharerWithResponse(response,
-        serverCallbackArgs: serverCallbackArgs);
+    return sharerWithResponse(response, serverCallbackArgs: serverCallbackArgs);
   }
 
   Future<Uri> scrapWithWeb(String url,
@@ -49,52 +46,33 @@ class LinkClient {
       Map<String, String>? serverCallbackArgs}) async {
     final response = await api.scrap(url,
         templateId: templateId, templateArgs: templateArgs);
-    return _sharerWithResponse(response,
-        serverCallbackArgs: serverCallbackArgs);
+    return sharerWithResponse(response, serverCallbackArgs: serverCallbackArgs);
   }
 
-  /// 카카오 디벨로퍼스에서 생성한 메시지 템플릿을 카카오톡으로 공유.
-  /// 템플릿을 생성하는 방법은 [메시지 템플릿 가이드](https://developers.kakao.com/docs/latest/ko/message/message-template) 참고.
+  /// Send KakaoLink messages with custom templates.
   Future<Uri> customWithTalk(int templateId,
       {Map<String, String>? templateArgs,
       Map<String, String>? serverCallbackArgs}) async {
     final response = await api.custom(templateId, templateArgs: templateArgs);
-    return _talkWithResponse(response, serverCallbackArgs: serverCallbackArgs);
+    return talkWithResponse(response, serverCallbackArgs: serverCallbackArgs);
   }
 
   Future<Uri> defaultWithTalk(DefaultTemplate template,
       {Map<String, String>? serverCallbackArgs}) async {
     final response = await api.defaultTemplate(template);
-    return _talkWithResponse(response, serverCallbackArgs: serverCallbackArgs);
+    return talkWithResponse(response, serverCallbackArgs: serverCallbackArgs);
   }
 
-  /// 카카오링크 컨텐츠 이미지로 활용하기 위해 원격 이미지를 카카오 이미지 서버로 업로드.
   Future<Uri> scrapWithTalk(String url,
       {int? templateId,
       Map<String, String>? templateArgs,
       Map<String, String>? serverCallbackArgs}) async {
     final response = await api.scrap(url,
         templateId: templateId, templateArgs: templateArgs);
-    return _talkWithResponse(response, serverCallbackArgs: serverCallbackArgs);
+    return talkWithResponse(response, serverCallbackArgs: serverCallbackArgs);
   }
 
-  Future<void> launchKakaoTalk(Uri uri) {
-    return _channel.invokeMethod("launchKakaoTalk", {"uri": uri.toString()});
-  }
-
-  /// 카카오링크 컨텐츠 이미지로 활용하기 위해 로컬 이미지를 카카오 이미지 서버로 업로드.
-  Future<ImageUploadResult> uploadImage(File image,
-      {bool secureResource = true}) async {
-    return await api.uploadImage(image, secureResource: secureResource);
-  }
-
-  /// 카카오링크 컨텐츠 이미지로 활용하기 위해 원격 이미지를 카카오 이미지 서버로 업로드.
-  Future<ImageUploadResult> scrapImage(String imageUrl,
-      {bool secureResource = true}) async {
-    return await api.scrapImage(imageUrl, secureResource: secureResource);
-  }
-
-  Future<Uri> _sharerWithResponse(LinkResult response,
+  Future<Uri> sharerWithResponse(LinkResult response,
       {Map<String, String>? serverCallbackArgs}) async {
     final params = {
       "app_key": KakaoContext.clientId,
@@ -113,7 +91,7 @@ class LinkClient {
         KakaoContext.hosts.sharer, "talk/friends/picker/easylink", params);
   }
 
-  Future<Uri> _talkWithResponse(LinkResult response,
+  Future<Uri> talkWithResponse(LinkResult response,
       {String? clientId, Map<String, String>? serverCallbackArgs}) async {
     final attachmentSize = await _attachmentSize(response,
         clientId: clientId, serverCallbackArgs: serverCallbackArgs);
@@ -134,6 +112,10 @@ class LinkClient {
     return Uri.parse(uri.toString().replaceAll('+', '%20'));
   }
 
+  Future<void> launchKakaoTalk(Uri uri) {
+    return _channel.invokeMethod("launchKakaoTalk", {"uri": uri.toString()});
+  }
+
   Future<Map<String, String?>> _extras(
       [Map<String, String>? serverCallbackArgs]) async {
     Map<String, String?> extras = {
@@ -151,6 +133,18 @@ class LinkClient {
     };
     extras.removeWhere((k, v) => v == null);
     return extras;
+  }
+
+  /// Upload the local image to the Kakao Image Server to use it as a KakaoLink content image.
+  Future<ImageUploadResult> uploadImage(File image,
+      {bool secureResource = true}) async {
+    return await api.uploadImage(image, secureResource: secureResource);
+  }
+
+  /// Upload remote image to Kakao Image Server to use as KakaoLink content image.
+  Future<ImageUploadResult> scrapImage(String imageUrl,
+      {bool secureResource = true}) async {
+    return await api.scrapImage(imageUrl, secureResource: secureResource);
   }
 
   Future<int> _attachmentSize(LinkResult response,
