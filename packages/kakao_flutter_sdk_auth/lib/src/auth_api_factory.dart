5eac04f6b95bc11a4123769efd936af9f2525bb0
mbkim95 <mbkim95@naver.com>
2021-12-06 15:35:05 +0900

    Implement kakao_flutter_sdk_auth package

---
+++ packages/kakao_flutter_sdk_auth/lib/src/auth_api_factory.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import 'package:kakao_flutter_sdk_auth/auth.dart';
+import 'package:kakao_flutter_sdk_auth/src/access_token_interceptor.dart';
+import 'package:kakao_flutter_sdk_auth/src/auth_api.dart';
+import 'package:kakao_flutter_sdk_auth/src/required_scopes_interceptor.dart';
+import 'package:kakao_flutter_sdk_common/common.dart';
+
+class AuthApiFactory {
+  /// [Dio] instance for Kakao OAuth server.
+  static final Dio kauthApi = _kauthApiInstance();
+
+  /// [Dio] instance for token-based Kakao API.
+  static final Dio authApi = _authApiInstance();
+
+  static Dio _kauthApiInstance() {
+    var dio = Dio();
+    dio.options.baseUrl = "https://${KakaoContext.hosts.kauth}";
+    dio.options.contentType = "application/x-www-form-urlencoded";
+    dio.interceptors.addAll([ApiFactory.kaInterceptor]);
+    return dio;
+  }
+
+  static Dio _authApiInstance() {
+    var dio = Dio();
+    dio.options.baseUrl = "https://${KakaoContext.hosts.kapi}";
+    dio.options.contentType = "application/x-www-form-urlencoded";
+    var tokenInterceptor = AccessTokenInterceptor(dio, AuthApi.instance);
+    var scopeInterceptor = RequiredScopesInterceptor(dio);
+    dio.interceptors
+        .addAll([tokenInterceptor, scopeInterceptor, ApiFactory.kaInterceptor]);
+    return dio;
+  }
+}
---
+++ packages/kakao_flutter_sdk_auth/lib/src/model/access_token_response.dart
@@ -0,0 +1,24 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'access_token_response.g.dart';
+
+/// @nodoc
+@JsonSerializable(fieldRename: FieldRename.snake, includeIfNull: false)
+class AccessTokenResponse {
+  String accessToken;
+  int expiresIn;
+  String? refreshToken;
+  int? refreshTokenExpiresIn;
+  String? scope;
+  String tokenType;
+  String? txId;
+
+  AccessTokenResponse(this.accessToken, this.expiresIn, this.refreshToken,
+      this.refreshTokenExpiresIn, this.scope, this.tokenType,
+      {this.txId});
+
+  factory AccessTokenResponse.fromJson(Map<String, dynamic> json) =>
+      _$AccessTokenResponseFromJson(json);
+
+  Map<String, dynamic> toJson() => _$AccessTokenResponseToJson(this);
+}
