1bfa54975e7a8f7b21d25a393fb6083dfb37f4ac
mbkim95 <mbkim95@naver.com>
2021-12-15 15:28:55 +0900

    Implement DebugPage to sample app

---
+++ example/lib/debug_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+
+class DebugPage extends StatefulWidget {
+  const DebugPage({Key? key}) : super(key: key);
+
+  @override
+  _DebugPageState createState() => _DebugPageState();
+}
+
+class _DebugPageState extends State<DebugPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Debug Page")),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.black,
+        child: FutureBuilder(
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) {
+                return const Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return const Center(child: Text('Error'));
+              }
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SingleChildScrollView(
+                    child: Text(
+                  '${snapshot.data}',
+                  style: TextStyle(color: Colors.green),
+                )),
+              );
+            },
+            future: SdkLog.logs),
+      ),
+    );
+  }
+}
