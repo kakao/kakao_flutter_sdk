e2b3491ab361b75fa6bebdaccc2409e0c6f6b52b
mbkim95 <mbkim95@naver.com>
2021-12-17 09:49:46 +0900

    Fix layout of FriendPage

--- example/lib/friend_page.dart
+++ example/lib/friend_page.dart
@@ -36,26 +36,40 @@ class _FriendPageState extends State<FriendPage> {
           )
         ],
       ),
-      body: ListView.separated(
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text(widget.items[index].label),
-              leading: widget.items[index].image != null &&
-                      widget.items[index].image != ''
-                  ? Image.network(widget.items[index].image!)
-                  : Icon(Icons.person),
-              trailing: Checkbox(
-                value: widget.items[index].checked,
-                onChanged: (value) {
-                  setState(() {
-                    widget.items[index].checked = value!;
-                  });
-                },
-              ),
-            );
-          },
-          separatorBuilder: (context, index) => const Divider(),
-          itemCount: widget.items.length),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(widget.items[index].label),
+                leading: widget.items[index].image != null &&
+                        widget.items[index].image != ''
+                    ? ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Image.network(widget.items[index].image!,
+                            width: 56, height: 56),
+                      )
+                    : Container(
+                        decoration: BoxDecoration(
+                          color: Theme.of(context).primaryColor,
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                        width: 56,
+                        height: 56,
+                        child: Icon(Icons.person)),
+                trailing: Checkbox(
+                  value: widget.items[index].checked,
+                  onChanged: (value) {
+                    setState(() {
+                      widget.items[index].checked = value!;
+                    });
+                  },
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => const Divider(),
+            itemCount: widget.items.length),
+      ),
     );
   }
 }
