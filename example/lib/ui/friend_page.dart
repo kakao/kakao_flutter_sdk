254a5939787074797009721a062ff931d843e2ed
mbkim95 <mbkim95@naver.com>
2021-12-24 15:22:45 +0900

    Change package of sample codes in sample app

--- example/lib/ui/friend_page.dart
+++ example/lib/ui/friend_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:kakao_flutter_sdk_example/picker_item.dart';
+import 'package:kakao_flutter_sdk_example/model/picker_item.dart';
 
 class FriendPage extends StatefulWidget {
   final List<PickerItem> items;
