fde0f7b5b41c65da96d3446f878af90280160f8e
tony.mb <tony.mb@kakaocorp.com>
2021-12-28 10:57:25 +0900

    Change DebugPage from StatefulWidget to StatelessWidget

--- example/lib/ui/debug_page.dart
+++ example/lib/ui/debug_page.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
-class DebugPage extends StatefulWidget {
+class DebugPage extends StatelessWidget {
   const DebugPage({Key? key}) : super(key: key);
 
-  @override
-  _DebugPageState createState() => _DebugPageState();
-}
-
-class _DebugPageState extends State<DebugPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
