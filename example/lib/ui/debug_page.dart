1522abcbeacd77c9543cddc3a536ba5bdd79b1fa
mbkim95 <mbkim95@naver.com>
2022-01-21 17:07:19 +0900

    Add clear debug log in sample app

--- example/lib/ui/debug_page.dart
+++ example/lib/ui/debug_page.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 
-class DebugPage extends StatelessWidget {
+class DebugPage extends StatefulWidget {
   const DebugPage({Key? key}) : super(key: key);
 
   @override
+  State<DebugPage> createState() => _DebugPageState();
+}
+
+class _DebugPageState extends State<DebugPage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Debug Page")),
+      appBar: AppBar(
+        title: Text("Debug Page"),
+        actions: [
+          GestureDetector(
+            child: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Center(
+                child: Text('CLEAR'),
+              ),
+            ),
+            onTap: () {
+              setState(() {
+                SdkLog.clear();
+              });
+            },
+          ),
+        ],
+      ),
       body: Container(
         width: double.infinity,
         height: double.infinity,
