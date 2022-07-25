d26a7d403e6ef6549bb0a2a9bac3dcfc1cb1d522
mbkim95 <mbkim95@naver.com>
2022-07-25 15:56:19 +0900

    Add a method to classify web browser to UaParser

--- packages/kakao_flutter_sdk_common/lib/src/web/ua_parser.dart
+++ packages/kakao_flutter_sdk_common/lib/src/web/ua_parser.dart
@@ -1,3 +1,14 @@
+enum Browser {
+  chrome,
+  safari,
+  naver,
+  daum,
+  kakaotalk,
+  firefox,
+  samsung,
+  unknown
+}
+
 class UaParser {
   final _androidRegExp = RegExp('^.*(android)', caseSensitive: false);
   final iOSRegExp = RegExp('iphone|ipod', caseSensitive: false);
@@ -14,5 +25,24 @@ class UaParser {
       return true;
     }
     return false;
+  }
+
+  Browser detectBrowser(String ua) {
+    if (ua.contains('KAKAOTALK')) {
+      return Browser.kakaotalk;
+    } else if (ua.contains('NAVER')) {
+      return Browser.naver;
+    } else if (ua.contains('Daum')) {
+      return Browser.daum;
+    } else if (ua.contains('Samsung')) {
+      return Browser.samsung;
+    } else if (ua.contains('FxiOS') || ua.contains("Firefox")) {
+      return Browser.firefox;
+    } else if (ua.contains('Chrome')) {
+      return Browser.chrome;
+    } else if (ua.contains('Safari')) {
+      return Browser.safari;
+    }
+    return Browser.unknown;
   }
 }
