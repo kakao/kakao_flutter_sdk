9eb44e751a90b2a205f32bfc67674991d522cbfd
mbkim95 <mbkim95@naver.com>
2022-08-10 12:33:18 +0900

    Refactor the codes for web login and navi

--- packages/kakao_flutter_sdk_common/lib/kakao_flutter_sdk_plugin.dart
+++ packages/kakao_flutter_sdk_common/lib/kakao_flutter_sdk_plugin.dart
@@ -1,11 +1,12 @@
 import 'dart:async';
-import 'dart:convert';
 import 'dart:html' as html;
 import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:kakao_flutter_sdk_common/src/web/login.dart';
+import 'package:kakao_flutter_sdk_common/src/web/navi.dart';
 import 'package:kakao_flutter_sdk_common/src/web/ua_parser.dart';
 
 class KakaoFlutterSdkPlugin {
@@ -99,13 +100,13 @@ class KakaoFlutterSdkPlugin {
               message:
                   'KakaoTalk easy login is only available on Android or iOS devices.');
         }
-        var arguments = call.arguments;
 
-        String fallbackUrl = _redirectLoginThroughWeb(Map.castFrom(arguments));
+        var arguments = call.arguments;
+        final kaHeader = _getKaHeader();
 
         if (_uaParser.isAndroid(userAgent)) {
-          String intent = _getAndroidLoginIntent(
-              userAgent, Map.castFrom(arguments), fallbackUrl);
+          String intent =
+              androidLoginIntent(kaHeader, userAgent, Map.castFrom(arguments));
 
           if (currentBrowser == Browser.kakaotalk ||
               currentBrowser == Browser.daum ||
@@ -115,11 +116,8 @@ class KakaoFlutterSdkPlugin {
             html.window.open(intent, '_blank');
           }
         } else if (_uaParser.isiOS(userAgent)) {
-          final isPopup = arguments[CommonConstants.isPopup];
-          String iosLoginScheme = _getIosLoginScheme(Map.castFrom(arguments));
-          String universalLink = isPopup
-              ? '${CommonConstants.iosWebUniversalLink}${Uri.encodeComponent(iosLoginScheme)}&web=${Uri.encodeComponent(fallbackUrl)}'
-              : '${CommonConstants.iosWebUniversalLink}${Uri.encodeComponent("${CommonConstants.iosInAppLoginScheme}?url=${Uri.encodeComponent(iosLoginScheme)}")}&web=${Uri.encodeComponent(iosLoginScheme)}';
+          final universalLink =
+              iosLoginUniversalLink(kaHeader, Map.castFrom(arguments));
 
           if (currentBrowser == Browser.safari) {
             html.window.open(universalLink, "_blank");
@@ -150,17 +148,15 @@ class KakaoFlutterSdkPlugin {
             'apiver=1.0&appkey=${KakaoSdk.appKey}&param=${Uri.encodeComponent(call.arguments['navi_params'])}&extras=${Uri.encodeComponent(call.arguments['extras'])}';
 
         if (_uaParser.isAndroid(userAgent)) {
-          html.window.location.href = _getAndroidNaviIntent(scheme, queries);
+          html.window.location.href = androidNaviIntent(scheme, queries);
           return true;
         } else if (_uaParser.isiOS(userAgent)) {
-          _bindPageHideEvent(_deferredFallback(
+          bindPageHideEvent(deferredFallback(
               'https://kakaonavi.kakao.com/launch/index.do?$queries',
               (storeUrl) {
             html.window.top?.location.href = storeUrl;
           }));
-
           html.window.location.href = '$scheme?$queries';
-
           return true;
         }
         return false;
@@ -201,164 +197,5 @@ class KakaoFlutterSdkPlugin {
       'end;'
     ].join(';');
     return intent;
-  }
-
-  String _getAndroidLoginIntent(
-      String userAgent, Map<String, dynamic> arguments, String fallbackUrl) {
-    Map<String, dynamic> extras = {
-      'channel_public_id': arguments['channel_public_ids'],
-      'service_terms': arguments['service_terms'],
-      'approval_type': arguments['approval_type'],
-      'prompt': arguments['prompt'],
-      'state': arguments['state'],
-    };
-    extras.removeWhere((k, v) => v == null);
-
-    var params = {
-      'client_id': KakaoSdk.appKey,
-      'approval_type': arguments['approval_type'],
-      'scope': arguments['scope'],
-      'auth_tran_id': arguments['state_token'],
-      'channel_public_id': arguments['channel_public_id'],
-      'prompt': arguments['prompt'],
-      'login_hint': arguments['login_hint'],
-      'nonce': arguments['nonce'],
-      'redirect_uri': arguments[CommonConstants.redirectUri],
-      'response_type': 'code',
-      'ka': _getKaHeader(),
-      'is_popup': 'true',
-      'extra.channel_public_id': arguments['channel_public_ids'],
-      'extra.service_terms': arguments['service_terms'],
-    };
-    params.removeWhere((k, v) => v == null);
-
-    String intent = [
-      'intent:#Intent',
-      'action=com.kakao.talk.intent.action.CAPRI_LOGGED_IN_ACTIVITY',
-      'launchFlags=0x08880000',
-      'S.com.kakao.sdk.talk.appKey=${KakaoSdk.appKey}',
-      'S.com.kakao.sdk.talk.redirectUri=${arguments['redirect_uri']}',
-      'S.com.kakao.sdk.talk.state=${arguments['state_token']}',
-      'S.com.kakao.sdk.talk.kaHeader=${_getKaHeader()}',
-      'S.com.kakao.sdk.talk.extraparams=${Uri.encodeComponent(jsonEncode(params))}',
-      'S.browser_fallback_url=${Uri.encodeComponent(fallbackUrl)}',
-      'end;',
-    ].join(';');
-    return intent;
-  }
-
-  String _getAndroidNaviIntent(String scheme, String queries) {
-    var url = '$scheme?$queries';
-
-    final intent = [
-      'intent:$url#Intent',
-      'package=com.locnall.KimGiSa',
-      'S.browser_fallback_url=${Uri.encodeComponent('https://kakaonavi.kakao.com/launch/index.do?$queries')}',
-      'end;'
-    ].join(';');
-    return intent;
-  }
-
-  String _getIosLoginScheme(Map<String, dynamic> arguments) {
-    final bool isPopup = arguments[CommonConstants.isPopup];
-
-    var authParams = {
-      'client_id': KakaoSdk.appKey,
-      'approval_type': arguments['approval_type'],
-      'scope': arguments['scope'],
-      'state': arguments['state_token'],
-      'channel_public_id': arguments['channel_public_id'],
-      'prompt': arguments['prompt'],
-      'device_type': arguments['device_type'],
-      'login_hint': arguments['login_hint'],
-      'nonce': arguments['nonce'],
-      'auth_tran_id': arguments['state_token'],
-      'extra.plus_friend_public_id': arguments['plus_friend_public_id'],
-      'extra.service_terms': arguments['service_terms'],
-      'redirect_uri': arguments['redirect_uri'],
-      'response_type': 'code',
-    };
-    authParams.removeWhere((k, v) => v == null);
-
-    var easyLoginAuthParms = {
-      ...authParams,
-      'is_popup': 'true',
-      'ka': _getKaHeader(),
-    };
-
-    var params = {
-      'client_id': KakaoSdk.appKey,
-      'redirect_uri': arguments[CommonConstants.redirectUri],
-      'response_type': 'code',
-      'is_popup': 'true',
-      'params': jsonEncode(authParams),
-    };
-    params.removeWhere((k, v) => v == null);
-
-    if (isPopup) {
-      return Uri.parse(CommonConstants.iosTalkLoginScheme)
-          .replace(queryParameters: params)
-          .toString();
-    }
-    return Uri.parse("https://kauth.kakao.com/oauth/authorize")
-        .replace(queryParameters: easyLoginAuthParms)
-        .toString();
-  }
-
-  String _redirectLoginThroughWeb(Map<String, dynamic> arguments) {
-    var params = {
-      'client_id': KakaoSdk.appKey,
-      'approval_type': arguments['approval_type'],
-      'scope': arguments['scope'],
-      'state': arguments['state_token'],
-      'channel_public_id': arguments['channel_public_id'],
-      'prompt': arguments['prompt'],
-      'login_hint': arguments['login_hint'],
-      'nonce': arguments['nonce'],
-      'redirect_uri': arguments[CommonConstants.redirectUri],
-      'response_type': 'code',
-      'ka': _getKaHeader(),
-      'is_popup': 'false',
-      'device_type': html.window.location.origin,
-      'extra.channel_public_id': arguments['channel_public_ids'],
-      'extra.service_terms': arguments['service_terms'],
-    };
-
-    params.removeWhere((k, v) => v == null);
-
-    return Uri.parse(
-            '${CommonConstants.scheme}://${KakaoSdk.hosts.kauth}/oauth/authorize')
-        .replace(queryParameters: params)
-        .toString();
-  }
-
-  Timer _deferredFallback(String storeUrl, Function(String) fallback) {
-    int timeout = 5000;
-
-    return Timer(Duration(milliseconds: timeout), () {
-      fallback(storeUrl);
-    });
-  }
-
-  _bindPageHideEvent(Timer timer) {
-    EventListener? listener;
-
-    listener = (event) {
-      if (!_isPageVisible()) {
-        timer.cancel();
-        html.window.removeEventListener('pagehide', listener);
-        html.window.removeEventListener('visibilitychange', listener);
-      }
-    };
-
-    html.window.addEventListener('pagehide', listener);
-    html.window.addEventListener('visibilitychange', listener);
-  }
-
-  bool _isPageVisible() {
-    if (html.document.hidden != null) {
-      return !html.document.hidden!;
-    }
-    return true;
   }
 }
---
+++ packages/kakao_flutter_sdk_common/lib/src/web/login.dart
@@ -0,0 +1,127 @@
+import 'dart:convert';
+
+import 'package:kakao_flutter_sdk_common/src/constants.dart';
+import 'package:kakao_flutter_sdk_common/src/kakao_sdk.dart';
+
+String androidLoginIntent(
+    String kaHeader, String userAgent, Map<String, dynamic> arguments) {
+  Map<String, dynamic> extras = {
+    'channel_public_id': arguments['channel_public_ids'],
+    'service_terms': arguments['service_terms'],
+    'approval_type': arguments['approval_type'],
+    'prompt': arguments['prompt'],
+    'state': arguments['state'],
+  };
+  extras.removeWhere((k, v) => v == null);
+
+  var params = {
+    'client_id': KakaoSdk.appKey,
+    'approval_type': arguments['approval_type'],
+    'scope': arguments['scope'],
+    'auth_tran_id': arguments['state_token'],
+    'channel_public_id': arguments['channel_public_id'],
+    'prompt': arguments['prompt'],
+    'login_hint': arguments['login_hint'],
+    'nonce': arguments['nonce'],
+    'redirect_uri': arguments['redirect_uri'],
+    'response_type': 'code',
+    'ka': kaHeader,
+    'is_popup': 'true',
+    'extra.channel_public_id': arguments['channel_public_ids'],
+    'extra.service_terms': arguments['service_terms'],
+  };
+  params.removeWhere((k, v) => v == null);
+
+  String intent = [
+    'intent:#Intent',
+    'action=com.kakao.talk.intent.action.CAPRI_LOGGED_IN_ACTIVITY',
+    'launchFlags=0x08880000',
+    'S.com.kakao.sdk.talk.appKey=${KakaoSdk.appKey}',
+    'S.com.kakao.sdk.talk.redirectUri=${arguments['redirect_uri']}',
+    'S.com.kakao.sdk.talk.state=${arguments['state_token']}',
+    'S.com.kakao.sdk.talk.kaHeader=$kaHeader',
+    'S.com.kakao.sdk.talk.extraparams=${Uri.encodeComponent(jsonEncode(params))}',
+    'S.browser_fallback_url=${Uri.encodeComponent(_redirectLoginThroughWeb(kaHeader, Map.castFrom(arguments)))}',
+    'end;',
+  ].join(';');
+  return intent;
+}
+
+String iosLoginUniversalLink(String kaHeader, Map<String, dynamic> arguments) {
+  final bool isPopup = arguments[CommonConstants.isPopup];
+  String fallbackUrl =
+      _redirectLoginThroughWeb(kaHeader, Map.castFrom(arguments));
+
+  String iosLoginScheme = isPopup
+      ? _iosPopupLoginScheme(kaHeader, arguments)
+      : _iosRedirectLoginScheme(kaHeader, arguments);
+
+  if (isPopup) {
+    return '${CommonConstants.iosWebUniversalLink}${Uri.encodeComponent(iosLoginScheme)}&web=${Uri.encodeComponent(fallbackUrl)}';
+  } else {
+    return '${CommonConstants.iosWebUniversalLink}${Uri.encodeComponent("${CommonConstants.iosInAppLoginScheme}?url=${Uri.encodeComponent(iosLoginScheme)}")}&web=${Uri.encodeComponent(iosLoginScheme)}';
+  }
+}
+
+String _iosPopupLoginScheme(String kaHeader, Map<String, dynamic> arguments) {
+  var params = {
+    'client_id': KakaoSdk.appKey,
+    'redirect_uri': arguments['redirect_uri'],
+    'response_type': 'code',
+    'is_popup': 'true',
+    'params': jsonEncode(_makeAuthParams(arguments)),
+  };
+  params.removeWhere((k, v) => v == null);
+
+  return Uri.parse(CommonConstants.iosTalkLoginScheme)
+      .replace(queryParameters: params)
+      .toString();
+}
+
+String _iosRedirectLoginScheme(
+    String kaHeader, Map<String, dynamic> arguments) {
+  var authParams = _makeAuthParams(arguments);
+
+  var easyLoginAuthParams = {
+    ...authParams,
+    'is_popup': 'true',
+    'ka': kaHeader,
+  };
+  return Uri.parse("https://kauth.kakao.com/oauth/authorize")
+      .replace(queryParameters: easyLoginAuthParams)
+      .toString();
+}
+
+String _redirectLoginThroughWeb(
+    String kaHeader, Map<String, dynamic> arguments) {
+  Map<String, dynamic> params = {
+    ..._makeAuthParams(arguments),
+    'ka': kaHeader,
+    'is_popup': 'false',
+  };
+  return Uri.parse(
+          '${CommonConstants.scheme}://${KakaoSdk.hosts.kauth}/oauth/authorize')
+      .replace(queryParameters: params)
+      .toString();
+}
+
+Map<String, dynamic> _makeAuthParams(Map<String, dynamic> arguments) {
+  var authParams = {
+    'client_id': KakaoSdk.appKey,
+    'approval_type': arguments['approval_type'],
+    'scope': arguments['scope'],
+    'state': arguments['state_token'],
+    'channel_public_id': arguments['channel_public_id'],
+    'prompt': arguments['prompt'],
+    // 'device_type': arguments['device_type'],
+    'login_hint': arguments['login_hint'],
+    'nonce': arguments['nonce'],
+    'auth_tran_id': arguments['state_token'],
+    'extra.plus_friend_public_id': arguments['plus_friend_public_id'],
+    'extra.service_terms': arguments['service_terms'],
+    'redirect_uri': arguments['redirect_uri'],
+    'response_type': 'code',
+  };
+  authParams.removeWhere((k, v) => v == null);
+  return authParams;
+}
