84e6497a9e4a89e54a3e037277c8cb0f1be5265f
mbkim95 <mbkim95@naver.com>
2022-08-31 15:19:13 +0900

    Fix the AccessTokenResponse class to be available outside of sdk

--- packages/kakao_flutter_sdk_auth/lib/kakao_flutter_sdk_auth.dart
+++ packages/kakao_flutter_sdk_auth/lib/kakao_flutter_sdk_auth.dart
@@ -5,6 +5,7 @@ export 'package:kakao_flutter_sdk_auth/src/access_token_interceptor.dart';
 export 'package:kakao_flutter_sdk_auth/src/auth_api.dart';
 export 'package:kakao_flutter_sdk_auth/src/auth_api_factory.dart';
 export 'package:kakao_flutter_sdk_auth/src/auth_code.dart';
+export 'package:kakao_flutter_sdk_auth/src/model/access_token_response.dart';
 export 'package:kakao_flutter_sdk_auth/src/model/cert_token_info.dart';
 export 'package:kakao_flutter_sdk_auth/src/model/oauth_token.dart';
 export 'package:kakao_flutter_sdk_auth/src/token_manager.dart';
