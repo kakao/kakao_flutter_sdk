4f33250152be89020766227cab28346edff249fa
mbkim95 <mbkim95@naver.com>
2022-09-29 16:26:26 +0900

    Fix the sample app to work on the web

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,8 +4,11 @@ import 'package:kakao_flutter_sdk_example/ui/debug_page.dart';
 import 'package:kakao_flutter_sdk_example/ui/main_page.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   KakaoSdk.init(
     nativeAppKey: '030ba7c59137629e86e8721eb1a22fd6',
+    javaScriptAppKey: 'fa2d8e9f47b88445000592c9a293bbe2',
     loggingEnabled: true,
   );
   runApp(MyApp());
