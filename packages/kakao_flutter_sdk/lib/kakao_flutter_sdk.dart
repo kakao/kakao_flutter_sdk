dd7a8539de55132ff7644d6cba227921c9fc9460
mbkim95 <mbkim95@naver.com>
2022-10-19 15:29:12 +0900

    Add friend package

--- packages/kakao_flutter_sdk/lib/kakao_flutter_sdk.dart
+++ packages/kakao_flutter_sdk/lib/kakao_flutter_sdk.dart
@@ -2,8 +2,9 @@ library kakao_flutter_sdk;
 
 export 'package:kakao_flutter_sdk_auth/kakao_flutter_sdk_auth.dart';
 export 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
-export 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
+export 'package:kakao_flutter_sdk_friend/kakao_flutter_sdk_friend.dart';
 export 'package:kakao_flutter_sdk_navi/kakao_flutter_sdk_navi.dart';
+export 'package:kakao_flutter_sdk_share/kakao_flutter_sdk_share.dart';
 export 'package:kakao_flutter_sdk_story/kakao_flutter_sdk_story.dart';
 export 'package:kakao_flutter_sdk_talk/kakao_flutter_sdk_talk.dart';
 export 'package:kakao_flutter_sdk_template/kakao_flutter_sdk_template.dart';
