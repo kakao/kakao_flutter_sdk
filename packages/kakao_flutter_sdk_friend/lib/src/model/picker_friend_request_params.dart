8787abb4ac98323007f0201eae18c79ef9f51cb4
mbkim95 <mbkim95@naver.com>
2022-12-26 12:23:38 +0900

    Fix document references in the PickerFriendRequestParams class

--- packages/kakao_flutter_sdk_friend/lib/src/model/picker_friend_request_params.dart
+++ packages/kakao_flutter_sdk_friend/lib/src/model/picker_friend_request_params.dart
@@ -20,18 +20,18 @@ class PickerFriendRequestParams {
   /// 선택한 친구 표시 여부 (멀티 피커에만 사용 가능)
   bool? showPickedFriend;
 
-  /// 선택 가능한 친구 수의 최대값
+  /// 선택 가능한 친구 수의 최대값 (멀티 피커에만 사용 가능)
   int? maxPickableCount;
 
-  /// 선택 가능한 친구 수의 최소값
+  /// 선택 가능한 친구 수의 최소값 (멀티 피커에만 사용 가능)
   int? minPickableCount;
 
-  /// flutter web 리다이렉트 방식 사용 시 필수
   /// 선택한 친구 정보를 받을 서비스 URL
+  /// flutter web 리다이렉트 방식 사용 시 필수
   String? returnUrl;
 
-  /// flutter web 리다이렉트 방식 또는 Android/iOS 앱에서만 사용 가능
   /// 뒤로가기 버튼 사용 여부 지정
+  /// flutter web 리다이렉트 방식 또는 Android/iOS 앱에서만 사용 가능
   bool? enableBackButton;
 
   PickerFriendRequestParams({
