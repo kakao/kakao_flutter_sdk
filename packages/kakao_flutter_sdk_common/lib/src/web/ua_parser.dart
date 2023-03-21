40c71e5f7426bd657e84420e2b386cfe9a83e282
mbkim95 <mbkim95@naver.com>
2023-03-21 11:53:49 +0900

    Fix the Facebook in-app browser identifier

--- packages/kakao_flutter_sdk_common/lib/src/web/ua_parser.dart
+++ packages/kakao_flutter_sdk_common/lib/src/web/ua_parser.dart
@@ -40,14 +40,14 @@ class UaParser {
       return Browser.samsung;
     } else if (ua.contains('FxiOS') || ua.contains("Firefox")) {
       return Browser.firefox;
+    } else if (ua.contains('FB_IAB')) {
+      return Browser.facebook;
+    } else if (ua.contains('Instagram')) {
+      return Browser.instagram;
     } else if (ua.contains('Chrome') || ua.contains('CriOS')) {
       return Browser.chrome;
     } else if (ua.contains('Safari')) {
       return Browser.safari;
-    } else if (ua.contains('FA_IAB')) {
-      return Browser.facebook;
-    } else if (ua.contains('Instagram')) {
-      return Browser.instagram;
     }
     return Browser.unknown;
   }
