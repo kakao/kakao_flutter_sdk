9440ebf0241c7fd302022288b384f074c1009629
mbkim95 <mbkim95@naver.com>
2023-06-28 16:47:31 +0900

    Seperate the prompt class from the auth_code.dart file

--- packages/kakao_flutter_sdk_auth/lib/src/auth_code.dart
+++ packages/kakao_flutter_sdk_auth/lib/src/auth_code.dart
@@ -11,6 +11,8 @@ import 'package:kakao_flutter_sdk_auth/src/utils.dart';
 import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
 import 'package:platform/platform.dart';
 
+import 'model/prompt.dart';
+
 const MethodChannel _channel = MethodChannel(CommonConstants.methodChannel);
 
 /// Kakao SDK의 카카오 로그인 내부 동작에 사용되는 클라이언트
@@ -293,17 +295,4 @@ class AuthCodeClient {
             sha512.convert(utf8.encode(UniqueKey().toString())).bytes)
         .split('=')[0];
   }
-}
-
-/// 동의 화면 요청 시 추가 상호작용을 요청하고자 할 때 전달하는 파라미터
-enum Prompt {
-  /// 기본 웹 브라우저에 카카오계정 cookie 가 이미 있더라도 이를 무시하고 무조건 로그인 화면을 보여주도록 함
-  login,
-
-  /// 카카오계정 가입 페이지로 이동 후, 카카오계정 가입 완료 후 동의 화면 출력
-  create,
-  selectAccount,
-
-  /// @nodoc
-  cert,
 }
