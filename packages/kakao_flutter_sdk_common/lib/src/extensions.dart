9eabd47fec2b5709197eb8377524802db81704b4
mbkim95 <mbkim95@naver.com>
2023-07-07 16:09:24 +0900

    fix: fix the logic of the toSnakeCase() function used internally by the SDK
    - Safari Browser does not support look-behind regular expression

--- packages/kakao_flutter_sdk_common/lib/src/extensions.dart
+++ packages/kakao_flutter_sdk_common/lib/src/extensions.dart
@@ -1,6 +1,12 @@
 extension StringExtensions on String {
   String toSnakeCase() {
-    final exp = RegExp('(?<=[a-z])[A-Z]');
-    return replaceAllMapped(exp, (match) => '_${match.group(0)}').toLowerCase();
+    String result = '';
+    for (var i = 0; i < length; i++) {
+      if (this[i].toUpperCase() == this[i] && i != 0) {
+        result += '_';
+      }
+      result += this[i].toLowerCase();
+    }
+    return result;
   }
 }
