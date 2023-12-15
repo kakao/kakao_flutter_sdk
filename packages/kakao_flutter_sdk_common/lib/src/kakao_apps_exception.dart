9d77fbe23642937a57218ef4f1426f5c3ed7b2ff
mbkim95 <mbkim95@naver.com>
2023-12-15 19:12:54 +0900

    feat: add AppsErrorCause

--- packages/kakao_flutter_sdk_common/lib/src/kakao_apps_exception.dart
+++ packages/kakao_flutter_sdk_common/lib/src/kakao_apps_exception.dart
@@ -44,6 +44,18 @@ enum AppsErrorCause {
   @JsonValue("KAE004")
   timeExpired,
 
+  /// 채널 정보를 확인할 수 없는 경우
+  @JsonValue("KAE005")
+  invalidChannel,
+
+  /// 채널이 추가 가능한 상태가 아닌 경우
+  @JsonValue("KAE006")
+  illegalStateChannel,
+
+  /// 브라우저를 지원하지 않는 경우
+  @JsonValue("KAE007")
+  unsupported,
+
   /// API를 사용할 수 없는 앱 타입인 경우
   @JsonValue("KAE101")
   appTypeError,
@@ -60,9 +72,13 @@ enum AppsErrorCause {
   @JsonValue("KAE104")
   appKeyTypeError,
 
+  /// 앱과 연결되지 않은 채널 정보로 API를 요청한 경우
+  @JsonValue("KAE105")
+  appChannelNotConnected,
+
   /// Access Token, KPIDT, 톡세션 등으로 앱 유저 인증에 실패하는 경우
   @JsonValue("KAE201")
-  authError,
+  authFailed,
 
   /// 앱에 연결되지 않은 유저가 API를 호출하는 경우
   @JsonValue("KAE202")
@@ -74,7 +90,7 @@ enum AppsErrorCause {
 
   /// API 사용에 필요한 계정 약관 동의가 되어 있지 않은 경우
   @JsonValue("KAE204")
-  accountTermsError,
+  accountTermsAgreeRequired,
 
   /// 계정 페이지에서 배송지 콜백으로 로그인 필요 응답을 전달하는 경우
   @JsonValue("KAE205")
