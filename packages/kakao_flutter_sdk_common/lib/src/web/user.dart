50aaaaa270596d476a292aaadb53ac28b19a56f4
mbkim95 <mbkim95@naver.com>
2024-01-23 17:37:52 +0900

    feat: implement selectShippingAddresses API in the web part

---
+++ packages/kakao_flutter_sdk_common/lib/src/web/user.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:kakao_flutter_sdk_common/src/kakao_sdk.dart';
+
+String createSelectShippingAddressesUrl(final Map<String, dynamic> params) {
+  return Uri.https(KakaoSdk.hosts.apps, '/user/address', params).toString();
+}
+
+String createKpidtUrl(final Map<String, dynamic> params) {
+  return Uri.https(KakaoSdk.hosts.apps, '/auth/kpidt', params).toString();
+}
