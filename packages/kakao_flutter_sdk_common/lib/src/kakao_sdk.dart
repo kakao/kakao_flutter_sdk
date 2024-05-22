2d64e76c175450d2df25b348d711196195695c7a
mbkim95 <mbkim95@naver.com>
2024-05-22 14:14:15 +0900

    Bump version to 1.9.2

--- packages/kakao_flutter_sdk_common/lib/src/kakao_sdk.dart
+++ packages/kakao_flutter_sdk_common/lib/src/kakao_sdk.dart
@@ -16,7 +16,7 @@ class KakaoSdk {
   static late String _nativeKey;
   static late String _jsKey;
 
-  static String sdkVersion = "1.9.1+2";
+  static String sdkVersion = "1.9.2";
 
   static String get appKey => kIsWeb ? _jsKey : _nativeKey;
 
