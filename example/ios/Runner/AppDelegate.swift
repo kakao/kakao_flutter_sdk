7b692a56dfd3f3984a16073e1521bf0056a81954
mbkim95 <mbkim95@naver.com>
2025-11-17 16:33:58 +0900

    refactor: remove unnecessary code in the iOS sample app

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -13,43 +13,5 @@ import Flutter
     
     func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
         GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
-
-        let channel = FlutterMethodChannel(name: "kakao.flutter.sdk.sample", binaryMessenger: engineBridge.applicationRegistrar.messenger())
-        
-        channel.setMethodCallHandler({
-            (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
-            guard call.method == "changePhase" else {
-                result(FlutterMethodNotImplemented)
-                return
-            }
-            self.changePhase()
-            result(call.method)
-        })
-    }
-    
-    func getWindow() -> UIWindow? {
-        return UIApplication.shared.keyWindow
-    }
-    
-    private func changePhase() {
-        showAlert()
-    }
-    
-    @IBAction private func showAlert() {
-        let controller = window?.rootViewController as! FlutterViewController
-        let alert = UIAlertController(title: "The app must be restarted", message: "The app will shut down.\nPlease run the app again.", preferredStyle: .alert)
-        let confirm = UIAlertAction(title: "OK", style: .default) { (action) in
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-                UIApplication.shared.perform(#selector(NSXPCConnection.suspend))
-                DispatchQueue.main.asyncAfter(deadline: .now()) {
-                    exit(0)
-                }
-            }
-        }
-        let close = UIAlertAction(title: "Cancel", style: .destructive, handler: nil)
-        
-        alert.addAction(close)
-        alert.addAction(confirm)
-        controller.present(alert, animated: true, completion: nil)
     }
 }
